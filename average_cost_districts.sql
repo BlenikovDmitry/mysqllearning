e459113b07e04c8887ede45cb4b84cd1d63dc963
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2025-08-28 16:58:57 +0300

    Create average_cost_districts.sql

---
+++ average_cost_districts.sql
@@ -0,0 +1,13 @@
+/* пример подсчитываем среднюю цену по всем квартирам всех районов */
+set @cost := 0;
+select avg(price) into @cost from flats;
+select @cost;
+/* подсчитываем среднюю цену всех квартир микрорайона Древлянка и вносим в таблицу districts */
+set @cost_dr = 0;
+select avg(price) into @cost_dr from flats where street in (select street from houses where dist = 'Древлянка');
+select truncate(@cost_dr, 2);
+update districts
+set avprice = @cost_dr where dist_name = 'Древлянка';
+select * from districts;
+
+/* надо будет довнести другие районы и написать код для обновления данных в них */
