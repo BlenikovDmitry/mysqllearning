8ff9d5fe68de6c18f27b87fab43b3b785e46cbc5
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2025-09-24 20:09:41 +0300

    Update average_cost_districts.sql

--- average_cost_districts.sql
+++ average_cost_districts.sql
@@ -2,13 +2,54 @@
 set @cost := 0;
 select avg(price) into @cost from flats;
 select @cost;
-/* подсчитываем среднюю цену всех квартир микрорайона Древлянка и вносим в таблицу districts 
-Древлянку можно поменять на что угодно */
-set @cost_dr = 0;
-select avg(price) into @cost_dr from flats where street in (select street from houses where dist = 'Древлянка');
-select truncate(@cost_dr, 2);
+/* подсчитываем среднюю цену всех квартир всех микрорайонов и вносим в таблицу districts  */
+set @cost_tmp = 0;
+select avg(price) into @cost_tmp from flats where street in (select street from houses where dist = 'Древлянка');
+select truncate(@cost_tmp, 2);
 update districts
-set avprice = @cost_dr where dist_name = 'Древлянка';
-select * from districts;
+set avprice = @cost_tmp where dist_name = 'Древлянка';
 
+set @cost_tmp = 0;
+select avg(price) into @cost_tmp from flats where street in (select street from houses where dist = 'Голиковка');
+select truncate(@cost_tmp, 2);
+update districts
+set avprice = @cost_tmp where dist_name = 'Голиковка';
+
+set @cost_tmp = 0;
+select avg(price) into @cost_tmp from flats where street in (select street from houses where dist = 'Железнодорожный');
+select truncate(@cost_tmp, 2);
+update districts
+set avprice = @cost_tmp where dist_name = 'Железнодорожный';
+
+set @cost_tmp = 0;
+select avg(price) into @cost_tmp from flats where street in (select street from houses where dist = 'Зарека');
+select truncate(@cost_tmp, 2);
+update districts
+set avprice = @cost_tmp where dist_name = 'Зарека';
+
+set @cost_tmp = 0;
+select avg(price) into @cost_tmp from flats where street in (select street from houses where dist = 'Кукковка');
+select truncate(@cost_tmp, 2);
+update districts
+set avprice = @cost_tmp where dist_name = 'Кукковка';
+
+set @cost_tmp = 0;
+select avg(price) into @cost_tmp from flats where street in (select street from houses where dist = 'Октябрьский');
+select truncate(@cost_tmp, 2);
+update districts
+set avprice = @cost_tmp where dist_name = 'Октябрьский';
+
+set @cost_tmp = 0;
+select avg(price) into @cost_tmp from flats where street in (select street from houses where dist = 'Перевалка');
+select truncate(@cost_tmp, 2);
+update districts
+set avprice = @cost_tmp where dist_name = 'Перевалка';
+
+set @cost_tmp = 0;
+select avg(price) into @cost_tmp from flats where street in (select street from houses where dist = 'Сулажгора');
+select truncate(@cost_tmp, 2);
+update districts
+set avprice = @cost_tmp where dist_name = 'Сулажгора';
+
+select * from districts;
 
